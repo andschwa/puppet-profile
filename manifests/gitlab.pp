b20e144dd02d254aca9248487b0d0f4b78518cb5
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-09-08 09:47:50 -0700

    Removing redis PPA (managed by module)

--- manifests/gitlab.pp
+++ manifests/gitlab.pp
@@ -8,16 +8,4 @@ class profile::gitlab {
   include postgresql::globals
   include postgresql::server
   include redis
-
-  case $::operatingsystem {
-    'Ubuntu': {
-      case $::operatingsystemrelease {
-        '12.04': {
-          apt::ppa { 'ppa:chris-lea/redis-server':
-            before => Package['redis']
-          }
-        }
-      }
-    }
-  }
 }

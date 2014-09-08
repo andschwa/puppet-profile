e28757f590aa685b0d74bd615e1628fb4839f1d4
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-09-08 09:43:17 -0700

    Moving ZNC to profile::irc_bouncer
    Added user and group resources. Added environment to supervisor program.

--- manifests/chat.pp
+++ manifests/chat.pp
@@ -2,27 +2,4 @@ class profile::chat {
   include mumble
   include prosody
 
-  package { 'znc': }
-
-  supervisor::program { 'znc':
-    command        => 'znc --foreground',
-    autorestart    => true,
-    user           => 'andrew',
-    group          => 'andrew',
-    directory      => '/home/andrew/',
-    stdout_logfile => '/home/andrew/.znc/stdout.log',
-    stderr_logfile => '/home/andrew/.znc/stderr.log',
-  }
-
-  case $::operatingsystem {
-    'Ubuntu': {
-      case $::operatingsystemrelease {
-        '12.04': {
-          apt::ppa { 'ppa:teward/znc':
-            before => Package['znc']
-          }
-        }
-      }
-    }
-  }
 }
---
+++ manifests/irc_bouncer.pp
@@ -0,0 +1,35 @@
+class profile::irc_bouncer {
+  package { 'znc': }
+  group { 'znc': }
+  user { 'znc':
+    comment    => 'User for IRC bouncer',
+    home       => '/home/znc',
+    managehome => true,
+    gid        => 'znc',
+    require    => Group['znc'],
+  }
+
+  supervisor::program { 'znc':
+    command        => 'znc --foreground',
+    autorestart    => true,
+    user           => 'znc',
+    group          => 'znc',
+    directory      => '/home/znc/',
+    stdout_logfile => '/var/log/supervisor/znc_stdout.log',
+    stderr_logfile => '/var/log/supervisor/znc_stderr.log',
+    environment    => 'HOME="/home/znc"',
+    require        => 'znc'
+  }
+
+  case $::operatingsystem {
+    'Ubuntu': {
+      case $::operatingsystemrelease {
+        '12.04': {
+          apt::ppa { 'ppa:teward/znc':
+            before => Package['znc']
+          }
+        }
+      }
+    }
+  }
+}
